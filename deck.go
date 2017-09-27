7cfc6b3d3532c30a8f76e0fd3a22b9cb6b36588f
Leandro Diniz <lerox@hotmail.com>
2017-09-26 23:26:51 -0300

    Tests

--- deck.go
+++ deck.go
@@ -29,7 +29,7 @@ func newDeck() deck {
 
     for _, suit := range cardSuits {
         for _, value := range cardValues {
-            cards = append(cards, suit+" of "+value)
+            cards = append(cards, value+" of "+suit)
         }
     }
 
