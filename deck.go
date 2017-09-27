cc08a9c0daf6a034c9a80e24c599620f910510e5
Leandro Diniz <lerox@hotmail.com>
2017-09-26 22:25:44 -0300

    First commit

---
+++ deck.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "os"
+    "strings"
+    "time"
+)
+
+// Create a new type of 'deck'
+// which is a slice of strings
+
+type deck []string
+
+func (d deck) print() { // receiver by convention 1-2 letters
+    for i, card := range d {
+        fmt.Println(i, card)
+    }
+}
+
+func newDeck() deck {
+    cards := deck{}
+
+    cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clu"}
+
+    cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+    for _, suit := range cardSuits {
+        for _, value := range cardValues {
+            cards = append(cards, suit+" of "+value)
+        }
+    }
+
+    return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+    return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+    return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+    return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func recoverFromFile(filename string) deck {
+    bs, err := ioutil.ReadFile(filename)
+
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+
+    return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffe() {
+
+    source := rand.NewSource(time.Now().UnixNano())
+    r := rand.New(source)
+
+    for i := range d {
+        newPosition := r.Intn(len(d) - 1)
+
+        d[i], d[newPosition] = d[newPosition], d[i]
+    }
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+    cards := newDeck()
+
+    cards.shuffe()
+    cards.print()
+}
