438822073d57e69b93ea641b2875369694eb7222
Leandro Diniz <lerox@hotmail.com>
2017-10-15 22:33:32 -0200

    Interfaces

---
+++ interfaces.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bot interface {
+    getGreeting() string
+}
+
+type englishBot struct {}
+type spanishBot struct {}
+
+func (englishBot) getGreeting() string {
+    return "Hi"
+}
+
+func printGreeting(b bot) {
+    fmt.Println(b.getGreeting())
+}
+
+func (spanishBot) getGreeting() string {
+    return "Hola"
+}
+
+
+func interfaces() {
+
+}
