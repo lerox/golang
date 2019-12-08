65e79bb005d6a5b30632726e08345d9bc375a207
Leandro Diniz <lerox@hotmail.com>
2019-12-08 20:33:53 -0300

    Code improvements

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -2,15 +2,14 @@ package sorting
 
 // BubbleSort algorithm implementation
 func BubbleSort(data []int) []int {
-
-	for j := len(data)-1; j>0; j-- {
+	for j := len(data) - 1; j > 0; j-- {
 
 		alreadySorted := true
 
 		for i := 0; i < j; i++ {
 			if data[i] > data[i+1] {
 				alreadySorted = false // I swapped, so...
-				data[i], data[i+1] = data[i+1], data[i]
+				data[i+1], data[i] = data[i], data[i+1]
 			}
 		}
 
